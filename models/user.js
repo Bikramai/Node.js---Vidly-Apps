14387edcf76c845e5970116c4ec0d6d3cd0a919d
bikramia <bikram.rhce@gmail.com>
2024-06-11 09:53:17 -0400

    Initial Commit

---
+++ models/user.js
@@ -0,0 +1,48 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+
+const User = mongoose.model('User', new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
+    }
+}));
+
+// function validateUser(user) { // This code me took to figure out a week!!!
+//     const schema = {
+//         name: Joi.string().min(5).max(50).required(),
+//         email: Joi.string().min(5).max(255).required().email(),
+//         password:Joi.string().max(255).required()
+//     };
+
+//     return Joi.validate(user, schema);
+// }
+
+function validateUser(user) {
+    const schema = Joi.object({
+        name: Joi.string().min(5).max(50).required(),
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required()
+    });
+
+    return schema.validate(user);
+}
+
+exports.User = User;
+exports.validate = validateUser;
